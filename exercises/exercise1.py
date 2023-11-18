763bd3c6ba89b5df4fc6b9e19c19ce5dbf908e32
Amir-Hussein-OTH <miro.apple94@gmail.com>
2023-11-18 12:07:12 +0100

    refactor(exersies): remove print statement in data processing script
    -The print statement in the data processing script was removed to enhance code cleanliness.
    -The job now runs successfully without unnecessary output.

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -4,19 +4,10 @@ from sqlalchemy import create_engine
 # URL of the CSV data
 data_url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 
-# Read CSV data into a DataFrame
 df = pd.read_csv(data_url, sep=";")
-
-# Print the DataFrame content
-print("DataFrame content:")
-print(df)
 
 # Create the SQLAlchemy engine
 engine = create_engine("sqlite:///airports.sqlite")
 
 # Write the DataFrame to the SQLite database
 df.to_sql("airports", engine, if_exists="replace", index=False)
-
-# Print a message indicating success
-print("Data successfully loaded and written to the 'airports' table in airports.sqlite.")
-

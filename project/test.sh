2d3322179afbe94368f57c117c9beffc000dcbbb
Amir-Hussein-OTH <miro.apple94@gmail.com>
2023-12-14 18:52:36 +0100

    Updated shell script to set the working directory, install dependencies, and execute the data processing pipeline."

--- project/test.sh
+++ project/test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # Set the working directory
-# shellcheck disable=SC2164
-cd testing
+cd testing || { echo "Unable to change directory. Exiting."; exit 1; }
 
 # Inform the user about the installation of dependencies
 echo "Installing project dependencies..."
-pip install -r requirements.txt
+pip install -r requirements.txt || { echo "Dependency installation failed. Exiting."; exit 1; }
 echo "Dependencies installed successfully."
 
-
+python ./data/pipeline.py
